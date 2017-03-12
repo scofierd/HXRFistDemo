ad90bd20b90e6b829eebf75e0515c41b6d6c0b82
Mac <mac@Tenda-911.local>
2017-03-12 23:38:23 +0800

    提交项目

---
+++ HXRFistDemo/HXRFistDemo/Test/Test.h
@@ -0,0 +1,15 @@
+//
+//  Test.h
+//  HXRFistDemo
+//
+//  Created by Mac on 2017/3/12.
+//  Copyright © 2017年 hxr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+- (NSString *)hh;
+
+@end
---
+++ HXRFistDemo/HXRFistDemo/Test/Test.m
@@ -0,0 +1,18 @@
+//
+//  Test.m
+//  HXRFistDemo
+//
+//  Created by Mac on 2017/3/12.
+//  Copyright © 2017年 hxr. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+- (NSString *)hh;
+{
+    return @"迈出第一步";
+}
+
+@end
